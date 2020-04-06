771ddd38e74b6733e8c688bdd5664943691f31e7
Arfel <rafael_1999_@hotmail.com.br>
2020-04-06 19:12:01 -0300

    commit 1

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -4,8 +4,95 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+// Configurando LEDs
+#define LED1_PIO			PIOA
+#define LED1_PIO_ID			ID_PIOA
+#define LED1_PIO_IDX		0
+#define LED1_PIO_IDX_MASK	(1 << LED1_PIO_IDX)
+
+#define LED2_PIO			PIOC
+#define LED2_PIO_ID			ID_PIOC
+#define LED2_PIO_IDX		30
+#define LED2_PIO_IDX_MASK	(1 << LED2_PIO_IDX)
+
+#define LED3_PIO			PIOB
+#define LED3_PIO_ID			ID_PIOB
+#define LED3_PIO_IDX		2
+#define LED3_PIO_IDX_MASK	(1 << LED3_PIO_IDX)
+
+// Configuracoes do botoes
+#define BUT1_PIO			PIOD
+#define BUT1_PIO_ID			16
+#define BUT1_PIO_IDX		28
+#define BUT1_PIO_IDX_MASK	(1u << BUT1_PIO_IDX)
+
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			10
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK	(1u << BUT3_PIO_IDX)
+
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			12
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK	(1u << BUT2_PIO_IDX)
+
+volatile char but1_flag;
+volatile char but2_flag;
+volatile char but3_flag;
+
+void but1_callback();
+void but2_callback();
+void but3_callback();
+void init_BUT();
+void init_LED();
+
+void but1_callback(void){
+	but1_flag = 1;
+}
+void but2_callback(void){
+	but2_flag = 1;
+}
+void but3_callback(void){
+	but3_flag = 1;
+}
+
+void init_LED(){
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+}
+
+void init_BUT(){
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK, PIO_PULLUP);
+	
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_PIO_IDX_MASK,
+	PIO_IT_RISE_EDGE,
+	but1_callback);
+	
+	pio_handler_set(BUT2_PIO,
+	BUT2_PIO_ID,
+	BUT2_PIO_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but2_callback);
+	
+	pio_handler_set(BUT3_PIO,
+	BUT3_PIO_ID,
+	BUT3_PIO_IDX_MASK,
+	PIO_IT_RISE_EDGE,
+	but3_callback);
+}
+
 int main (void)
 {
+	init_LED();
 	board_init();
 	sysclk_init();
 	delay_init();
