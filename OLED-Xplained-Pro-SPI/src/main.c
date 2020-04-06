5470bd1910320d132b645877888cf314c6c264f8
Arfel <rafael_1999_@hotmail.com.br>
2020-04-06 16:22:49 -0300

    Update main.c

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -39,6 +39,7 @@
 volatile char but1_flag;
 volatile char but2_flag;
 volatile char but3_flag;
+volatile Bool f_rtt_alarme = false;
 
 void but1_callback();
 void but2_callback();
@@ -46,14 +47,40 @@ void but3_callback();
 void init_BUT();
 void init_LED();
 
-void but1_callback(void){
+void but1_callback(){
 	but1_flag = 1;
 }
-void but2_callback(void){
+void but2_callback(){
 	but2_flag = 1;
 }
-void but3_callback(void){
+void but3_callback(){
 	but3_flag = 1;
+}
+
+void pin_toggle(Pio *pio, uint32_t mask){
+	if(pio_get_output_data_status(pio, mask))
+	pio_clear(pio, mask);
+	else
+	pio_set(pio,mask);
+}
+
+void RTT_Handler(){
+	uint32_t ul_status;
+
+	/* Get RTT status - ACK */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
+		//f_rtt_alarme = false;
+		pin_toggle(LED2_PIO, LED2_PIO_IDX_MASK);    // BLINK Led
+	}
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		// pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
 }
 
 void init_LED(){
@@ -90,22 +117,61 @@ void init_BUT(){
 	but3_callback);
 }
 
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN | RTT_MR_RTTINCIEN);
+}
+
+
 int main (void)
 {
+	WDT->WDT_MR=WDT_MR_WDDIS;
 	init_LED();
 	board_init();
 	sysclk_init();
 	delay_init();
 
-  // Init OLED
+	// Init OLED
 	gfx_mono_ssd1306_init();
-  
-  // Escreve na tela um circulo e um texto
+	
+	// Escreve na tela um circulo e um texto
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	gfx_mono_draw_string("mundo", 50,16, &sysfont);
 
-  /* Insert application code here, after the board has been initialized. */
+	/* Insert application code here, after the board has been initialized. */
+	int freq_LED1 = 0;
+	int freq_LED2 = 0;
+	int freq_LED3 = 1;
+	
 	while(1) {
-
+		if (f_rtt_alarme){
+			
+			/*
+			* IRQ apos 4s -> 8*0.5
+			*/
+			uint16_t pllPreScale = (int) (((float) 32768) / 4.0);
+			uint32_t irqRTTvalue = 8;
+			
+			// reinicia RTT para gerar um novo IRQ
+			RTT_init(pllPreScale, irqRTTvalue);
+			
+			f_rtt_alarme = false;
+		}
 	}
 }
